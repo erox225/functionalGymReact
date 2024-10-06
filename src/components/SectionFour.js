8de92157a0c42ddad8fa5c272d9f37e595db6f6e
Alejandro <noesvenezuela@gmail.com>
2024-10-06 20:42:45 +0200

    Mejoras en general

--- src/components/SectionFour.js
+++ src/components/SectionFour.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUsers, faHeartbeat, faClock, faBolt } from '@fortawesome/free-solid-svg-icons'; // Íconos
 import './css/SectionFour.css'; // Estilos para esta sección
@@ -6,12 +6,12 @@ import './css/SectionFour.css'; // Estilos para esta sección
 // Datos de actividades por fecha
 const activitiesByDate = {
   "2024-10-06": [
-    { name: 'Gimnasio', time: '09:00', aforoMax: 30, aforoActual: 15, intensidad: 'Alta', duracion: "60'" },
-    { name: 'Reunión de equipo', time: '11:00', aforoMax: 10, aforoActual: 5, intensidad: 'Media', duracion: "45'" },
+    { name: 'Gimnasio', time: '09:00', aforoMax: 30, aforoActual: 15, intensidad: 'Alta', duracion: "60'", tipo: 'Fuerza - Intensidad' },
+    { name: 'Reunión de equipo', time: '11:00', aforoMax: 10, aforoActual: 5, intensidad: 'Media', duracion: "45'", tipo: 'Mente - Cuerpo' },
   ],
   "2024-10-07": [
-    { name: 'Yoga', time: '08:00 - 09:00', aforoMax: 20, aforoActual: 12, intensidad: 'Baja', duracion: "60'" },
-    { name: 'Clase de inglés', time: '10:00', aforoMax: 25, aforoActual: 20, intensidad: 'Media', duracion: "50'" },
+    { name: 'Yoga', time: '08:00 - 09:00', aforoMax: 20, aforoActual: 12, intensidad: 'Baja', duracion: "60'", tipo: 'Mente - Cuerpo' },
+    { name: 'Clase de inglés', time: '10:00', aforoMax: 25, aforoActual: 20, intensidad: 'Media', duracion: "50'", tipo: 'Mente - Cuerpo' },
   ],
   // Añade más fechas y actividades según necesites
 };
@@ -49,15 +49,13 @@ const SectionFour = () => {
 
   const weekDays = getWeekDays();
 
-  // Función para determinar la clase de color basada en la intensidad
-  const getIntensityClass = (intensidad) => {
-    switch (intensidad) {
-      case 'Alta':
-        return 'intensidad-alta';
-      case 'Media':
-        return 'intensidad-media';
-      case 'Baja':
-        return 'intensidad-baja';
+  // Función para determinar la clase de color basada en el tipo de actividad
+  const getTipoClass = (tipo) => {
+    switch (tipo) {
+      case 'Fuerza - Intensidad':
+        return 'tipo-fuerza';
+      case 'Mente - Cuerpo':
+        return 'tipo-mente';
       default:
         return '';
     }
@@ -84,7 +82,7 @@ const SectionFour = () => {
         <h2 className="sectionFour-schedule-title">{new Date(selectedDate).toLocaleDateString('es-ES', { weekday: 'long' })}</h2>
         {activities.length > 0 ? (
           activities.map((activity, index) => (
-            <div key={index} className="sectionFour-class-item">
+            <div key={index} className={`sectionFour-class-item ${getTipoClass(activity.tipo)}`}>
               <div className="sectionFour-class-time">
                 <FontAwesomeIcon icon={faClock} />  {activity.time}
               </div>
--- src/components/SectionThree.js
+++ src/components/SectionThree.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ModalTrainer from './ModalTrainer';
 import './css/SectionThree.css'; // Archivo CSS separado para los estilos
 
 const SectionThree = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTrainer, setSelectedTrainer] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0); // Para el carrusel de imágenes
+  const [touchStart, setTouchStart] = useState(null); // Para manejar el deslizamiento táctil
+  const timeoutRef = useRef(null);
 
   const handleOpenModal = (trainer) => {
     setSelectedTrainer(trainer);
@@ -16,34 +19,60 @@ const SectionThree = () => {
     setSelectedTrainer(null);
   };
 
+  // Para el deslizamiento táctil
+  const handleTouchStart = (e) => {
+    const touchStartX = e.touches[0].clientX;
+    setTouchStart(touchStartX); // Asignar el valor inicial del toque
+  };
+
+  const handleTouchMove = (e) => {
+    if (!touchStart) return;
+    const touchEndX = e.touches[0].clientX;
+    if (touchStart - touchEndX > 50) {
+      nextSlide(); // Deslizar a la derecha
+    }
+    if (touchStart - touchEndX < -50) {
+      prevSlide(); // Deslizar a la izquierda
+    }
+  };
+
+  // Controlar el cambio automático de las imágenes
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(() => {
+      nextSlide();
+    }, 5000); // Cada 5 segundos
+
+    return () => {
+      resetTimeout();
+    };
+  }, [currentIndex]);
+
+  const resetTimeout = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % 5); // Número de imágenes en el carrusel
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? 4 : prevIndex - 1)); // Número de imágenes en el carrusel
+  };
+
   return (
     <section className="section-three">
       <div className="section-three-container">
-        {/* Primera sección - INFO */}
-        <div className="section-three-info">
-          <h2 className="section-three-title">INFO</h2>
-          <p className="section-three-description">
-            Train now es un estudio fitness que te ofrece una nueva experiencia de clases colectivas,
-            estamos dispuestos a llevarte a tu máximo potencial mejorando tu salud física y mental,
-            mientras entrenas, aprendes y disfrutas.
-          </p>
-        </div>
-        <div className="section-three-image">
-          <img src="/images/fitness-image.jpg" alt="Fitness Training" className="info-image" />
-        </div>
-      </div>
-
-      <div className="section-three-container">
         {/* Segunda sección - QUIENES SOMOS */}
         <div className="section-three-image clickable" onClick={() => handleOpenModal('Miguel Labrador')}>
-          {/* Imagen de fondo definida por CSS */}
           <div className="trainer-image trainer-one"></div>
-          <p className='section-three-trainer-name'>Miguel Labrador</p>
+          <p className="section-three-trainer-name">Miguel Labrador</p>
         </div>
         <div className="section-three-image clickable" onClick={() => handleOpenModal('Jessica Di Maggio')}>
-          {/* Imagen de fondo definida por CSS */}
           <div className="trainer-image trainer-two"></div>
-          <p className='section-three-trainer-name'>Jessica Di Maggio</p>
+          <p className="section-three-trainer-name">Jessica Di Maggio</p>
         </div>
         <div className="section-three-info">
           <h2 className="section-three-title">QUIENES SOMOS</h2>
@@ -55,10 +84,30 @@ const SectionThree = () => {
         </div>
       </div>
 
+      <div className="section-three-container">
+        {/* Primera sección - INFO */}
+        <div className="section-three-info">
+          <h2 className="section-three-title">INFO</h2>
+          <p className="section-three-description">
+            Train now es un estudio fitness que te ofrece una nueva experiencia de clases colectivas,
+            estamos dispuestos a llevarte a tu máximo potencial mejorando tu salud física y mental,
+            mientras entrenas, aprendes y disfrutas.
+          </p>
+        </div>
+
+        {/* Carrusel de imágenes */}
+        <div className="carousel-container" onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}>
+          {[0, 1, 2, 3, 4].map((index) => (
+            <div
+              key={index}
+              className={`carousel-slide slide-${index} ${index === currentIndex ? 'active' : ''}`}
+            />
+          ))}
+        </div>
+      </div>
+
       {/* Modal */}
-      {isModalOpen && (
-        <ModalTrainer trainer={selectedTrainer} onClose={handleCloseModal} />
-      )}
+      {isModalOpen && <ModalTrainer trainer={selectedTrainer} onClose={handleCloseModal} />}
     </section>
   );
 };
